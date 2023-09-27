a19eea78db2242019b72dae6af74fea16e11f9c1
raimundo mena <raimundomena@200-177-123-eduroam-al.wlan.net.uc.cl>
2023-09-27 12:09:55 -0300

    fix: .env types

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -6,7 +6,7 @@ import { addDoc, collection } from "firebase/firestore";
 const List = ({ navigation }: any) => {
   const addTodo = async () => {
     const docRef = await addDoc(collection(FIREBASE_DB, "todos"), {
-      title: "I am a test",
+      title: "I am a test, created at: " + new Date().toISOString(),
       done: false,
     });
     console.log("Document written with ID: ", docRef.id);
