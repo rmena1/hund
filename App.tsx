f58a68259a6c6eef7e9a1961f63fd84c81249002
unknown <nlewin@uc.cl>
2023-10-05 09:25:52 -0300

    bottom-bar-structure

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import * as Font from 'expo-font';
-import fonts from './config/fonts';
-import { useState } from 'react';
-import AppLoading from 'expo-app-loading';
+import * as Font from "expo-font";
+import fonts from "./config/fonts";
+import { useState } from "react";
+import AppLoading from "expo-app-loading";
+import Navigation from "./app/navigation/index";
 
 // screens
 import StartScreen from "./app/screens/StartScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import CreateUserScreen from "./app/screens/CreateUserScreen";
+import List from "./app/screens/List";
 
 const Stack = createNativeStackNavigator();
 
@@ -31,30 +33,5 @@ export default function App() {
     );
   }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen 
-          name="StartScreen" 
-          component={StartScreen}  
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="LoginScreen"
-          component={LoginScreen}  
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="RegisterScreen" 
-          component={RegisterScreen}  
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen 
-          name="CreateUserScreen" 
-          component={CreateUserScreen}  
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
---
+++ app/navigation/tabs.tsx
@@ -0,0 +1,130 @@
+import { Text, Platform, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import List from "../screens/List";
+import Details from "../screens/Details";
+import { Colors, Fonts } from "../styles/generalStyles";
+
+// Thanks for watching
+const Tab = createBottomTabNavigator();
+// const screenOptions = {
+//   tabBarShowLabel: false,
+//   headerShown: false,
+//   tabBarStyle: {
+//     position: "absolute",
+//     bottom: 0,
+//     right: 0,
+//     left: 0,
+//     elevation: 0,
+//     height: 60,
+//     background: "#fff",
+//   },
+// };
+function TabsBar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="list"
+        component={List}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 10,
+                }}
+              >
+                <Ionicons
+                  name="home-outline"
+                  size={24}
+                  color={focused ? Colors.orange : Colors.gray1}
+                />
+              </View>
+            );
+          },
+          tabBarLabel: "",
+        }}
+      />
+      <Tab.Screen
+        name="details"
+        component={Details}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 10,
+                }}
+              >
+                <Ionicons
+                  name="clipboard-outline"
+                  size={24}
+                  color={focused ? Colors.orange : Colors.gray1}
+                />
+              </View>
+            );
+          },
+          tabBarLabel: "",
+        }}
+      />
+      <Tab.Screen
+        name="details2"
+        component={Details}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 10,
+                }}
+              >
+                <Ionicons
+                  name="chatbox-ellipses-outline"
+                  size={24}
+                  color={focused ? Colors.orange : Colors.gray1}
+                />
+              </View>
+            );
+          },
+          tabBarLabel: "",
+        }}
+      />
+      <Tab.Screen
+        name="details3"
+        component={Details}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 10,
+                }}
+              >
+                <Ionicons
+                  name="settings-outline"
+                  size={24}
+                  color={focused ? Colors.orange : Colors.gray1}
+                />
+              </View>
+            );
+          },
+          tabBarLabel: "",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default TabsBar;
