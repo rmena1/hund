e87cf934c2d2502035a38f8e693b4c510547d73c
raimundo mena <rma@uc.cl>
2023-10-18 09:19:47 -0300

    fix: fixed fonts and list index

--- app/screens/Walks.tsx
+++ app/screens/Walks.tsx
@@ -10,104 +10,92 @@ const Walks = ({ navigation }: any) => {
 
   useEffect(() => {
     console.log(dogs.length);
-}, [dogs])
+  }, [dogs]);
 
-  useEffect (() => {
+  useEffect(() => {
     const walksRef = collection(FIREBASE_DB, "paseos");
     const dogsRef = collection(FIREBASE_DB, "dogData");
 
     const walkSubscriber = onSnapshot(walksRef, {
-        next: (snapshot) => {
-            const walks: any[] = [];
-            snapshot.docs.forEach(doc => {
-                walks.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-            });
-            setWalks(walks);
-        },
-    })
+      next: (snapshot) => {
+        const walks: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          walks.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setWalks(walks);
+      },
+    });
 
     const dogSubscriber = onSnapshot(dogsRef, {
-        next: (snapshot) => {
-            const updatedDogs = snapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            setDogs(updatedDogs);
-        },
+      next: (snapshot) => {
+        const updatedDogs = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setDogs(updatedDogs);
+      },
     });
 
     return () => {
-        walkSubscriber();
-        dogSubscriber();
-    }
-  
-}, [])
+      walkSubscriber();
+      dogSubscriber();
+    };
+  }, []);
 
-return (
+  return (
     <View style={walksStyles.page}>
-        <ScrollView>
+      <ScrollView>
         <View style={walksStyles.contentContainer}>
-            {walks.map((walk: any) => {
-                const walkDate = walk.fecha.toDate();
-                const date = walkDate.toLocaleDateString();
-                const time = walkDate.toLocaleTimeString();
-                if (walk.email_usuario){
-                    return (
-                        <View style={walksStyles.walkItem}>
-                            <Text style={walksStyles.infoContainer}>
-                                <Text style={walksStyles.walkTitles}>
-                                    Email: 
-                                </Text>
-                                <Text>{' '}{walk.email_usuario}</Text>
-                            </Text>
-                            <Text style={walksStyles.infoContainer}>
-                                <Text style={walksStyles.walkTitles}>
-                                    Nombre perro: 
-                                </Text>
-                                <Text style={walksStyles.walkInfo}>
-                                    {' '}{dogs[walk.perro] ? dogs[walk.perro].name : "Sin nombre"}
-                                </Text>
-                            </Text>
-                            <Text style={walksStyles.infoContainer}>
-                                <Text style={walksStyles.walkTitles}>
-                                    Fecha: 
-                                </Text>
-                                <Text>{' '}{date}</Text>
-                            </Text>
-                            <Text style={walksStyles.infoContainer}>
-                                <Text style={walksStyles.walkTitles}>
-                                    Hora de pedido: 
-                                </Text>
-                                <Text>{' '}{time}</Text>
-                            </Text>
-                            <Text style={walksStyles.infoContainer}>
-                                <Text style={walksStyles.walkTitles}>
-                                    Tipo: 
-                                </Text>
-                                <Text>
-                                    {' '}{walk.inmediato ? "Inmediato" : "Programado"}
-                                </Text>
-                            </Text>
-                            <View style={walksStyles.viewMoreContainer}>
-                                <View style={walksStyles.viewMoreBox}>
-                                    <View style={walksStyles.arrow}></View>
-                                </View>
-                            </View>
-                        </View>
-                    );
-                }
-            })}
-      </View>
+          {walks.map((walk: any, index: number) => {
+            const walkDate = walk.fecha.toDate();
+            const date = walkDate.toLocaleDateString();
+            const time = walkDate.toLocaleTimeString();
+            if (walk.email_usuario) {
+              return (
+                <View key={index} style={walksStyles.walkItem}>
+                  <Text style={walksStyles.infoContainer}>
+                    <Text style={walksStyles.walkTitles}>Email:</Text>
+                    <Text> {walk.email_usuario}</Text>
+                  </Text>
+                  <Text style={walksStyles.infoContainer}>
+                    <Text style={walksStyles.walkTitles}>Nombre perro:</Text>
+                    <Text style={walksStyles.walkInfo}>
+                      {" "}
+                      {dogs[walk.perro] ? dogs[walk.perro].name : "Sin nombre"}
+                    </Text>
+                  </Text>
+                  <Text style={walksStyles.infoContainer}>
+                    <Text style={walksStyles.walkTitles}>Fecha:</Text>
+                    <Text> {date}</Text>
+                  </Text>
+                  <Text style={walksStyles.infoContainer}>
+                    <Text style={walksStyles.walkTitles}>Hora de pedido:</Text>
+                    <Text> {time}</Text>
+                  </Text>
+                  <Text style={walksStyles.infoContainer}>
+                    <Text style={walksStyles.walkTitles}>Tipo:</Text>
+                    <Text> {walk.inmediato ? "Inmediato" : "Programado"}</Text>
+                  </Text>
+                  <View style={walksStyles.viewMoreContainer}>
+                    <View style={walksStyles.viewMoreBox}>
+                      <View style={walksStyles.arrow}></View>
+                    </View>
+                  </View>
+                </View>
+              );
+            }
+          })}
+        </View>
       </ScrollView>
       <Image
-          source={require("../assets/images/home_dog.png")}
-          style={walksStyles.imageWalks}
-        />
+        source={require("../assets/images/home_dog.png")}
+        style={walksStyles.imageWalks}
+      />
     </View>
-  );  
+  );
 };
 
 export default Walks;
